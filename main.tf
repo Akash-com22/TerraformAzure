cb2fa61fe570c8628a1d3b8146efebeb5cd4c4bd
Akash-com22 <82569666+Akash-com22@users.noreply.github.com>
2022-01-04 17:24:51 +0530

    Update main.tf
    Variables are removed

--- main.tf
+++ main.tf
@@ -19,15 +19,15 @@ resource "azurerm_resource_group" "resource_rg" {
   location = "East US"
 }
 resource "azurerm_virtual_network" "main" {
-  name                = "${var.prefix}-network"
+  name                = "virtual-network"
   address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.resource_rg.location
-  resource_group_name = azurerm_resource_group.resource_rg.name
+  location            = "East US"
+  resource_group_name = "RG-Container"
 }
 resource "azurerm_subnet" "internal" {
   name                 = "internal"
-  resource_group_name  = azurerm_resource_group.resource_rg.name
-  virtual_network_name = azurerm_virtual_network.main.name
+  resource_group_name  = "RG-Container"
+  virtual_network_name = "virtual-network"
   address_prefixes     = ["10.0.2.0/24"]
 }
 
