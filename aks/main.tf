b8ff5ecec0d6a62d98a4904f60d6031beeaecf44
Akash-com22 <82569666+Akash-com22@users.noreply.github.com>
2022-01-06 17:12:49 +0530

    Update main.tf
    Updated

--- aks/main.tf
+++ aks/main.tf
@@ -1,12 +1,3 @@
-# Azure Provider source and version being used
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "=2.46.0"
-    }
-  }
-}
 
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
