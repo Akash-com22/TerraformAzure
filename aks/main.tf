8876d7dedc2bbbb8beaff65fb1aabd70608e155b
Akash-com22 <82569666+Akash-com22@users.noreply.github.com>
2022-01-10 13:52:15 +0530

    Update main.tf
    updated the name of resources

--- aks/main.tf
+++ aks/main.tf
@@ -12,13 +12,13 @@ provider "azurerm" {
   features {}
 }
 resource "azurerm_virtual_network" "main" {
-  name                = "virtual-network"
+  name                = "virtual-network-demo"
   address_space       = ["10.0.0.0/16"]
   location            = "East US"
   resource_group_name = "RG-Container"
 }
 resource "azurerm_subnet" "internal" {
-  name                 = "internal"
+  name                 = "internal-demo"
   resource_group_name  = "RG-Container"
   virtual_network_name = "virtual-network"
   address_prefixes     = ["10.0.2.0/24"]
